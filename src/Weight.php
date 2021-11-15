31569f222f73acba33d82b148315eb8ced3e13dd
Ahmed AlQahtani <alq.ahmad@gmail.com>
2021-11-15 23:32:51 +0300

    test for varient php versions

--- src/Weight.php
+++ src/Weight.php
@@ -5,7 +5,7 @@ namespace Alqahtani\WeightConversions;
 class Weight
 {
     protected float $kilograms;
-    
+
     public static function kilograms(float $kilograms): self
     {
         return new static($kilograms);
@@ -13,6 +13,7 @@ class Weight
 
     public function __construct(float $kilograms)
     {
+        $this->kilograms = $kilograms;
     }
 
     public function toPounds(): float
